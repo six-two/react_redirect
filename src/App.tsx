b75214b7392be8dc0e915340920157fbec0ab4c9
six-two <info@six-two.dev>
2020-06-16 16:50:30 +1200

    Created simple working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// TODOs
+// Add a tutorial / instructions
+// Make it look nice
+// Add debugging features
+// Add (more) templates
+// Add this to my projects page. Write about stateless web apps on my projects page
+
+class App extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      url: "https://example.com",
+      template: "#type=email&action=unsubscribe&userId=%s&campaign=Summer2020",
+    }
+  }
+
+  render() {
+    let encoded = "LNK" + this.uriSafeEncode(this.state.url);
+    let link = "follow.html"
+    // let link = "file:///home/user/c/react_redirect/public/follow.html"
+    link += this.state.template.replace("%s", encoded);
+
+    return (
+      <div className="App">
+      <h1>Create a (deceptive) redirect link</h1>
+      URL:<input type="text" onChange={this.onUrlChange} value={this.state.url} />
+      <hr />
+      Template:<input type="text" onChange={this.onTemplateChange} value={this.state.template} />
+      <hr />
+      Created link: <a target="_blank" rel="noopener noreferrer" href={link}>{link}</a>
+      </div>
+    );
+  }
+
+  onUrlChange = (e: any) => {
+    this.setState({url: e.target.value});
+  }
+
+  onTemplateChange = (e: any) => {
+    this.setState({template: e.target.value});
+  }
+
+  uriSafeEncode(data: string): string {
+    const base64 = btoa(data);
+    return base64
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=/g, '');
+  }
+}
+
+interface State {
+  url: string,
+  template: string,
 }
 
 export default App;
